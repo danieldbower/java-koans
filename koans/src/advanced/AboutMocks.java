495acc1e1f74a2abed673ad52bdd3a441ca509aa
Daniel Bower <dbower@updox.com>
2017-01-12 22:20:16 -0500

    Advanced and Java7 solutions

--- koans/src/advanced/AboutMocks.java
+++ koans/src/advanced/AboutMocks.java
@@ -35,12 +35,18 @@ public class AboutMocks {
         }
     }
 
+
+
     @Koan
     public void simpleAnonymousMock() {
         // HINT: pass a safe Collaborator implementation to constructor
         // new ClassUnderTest(new Collaborator(){... it should not be the
         // objective of this test to test that collaborator, so replace it
-        new ClassUnderTest().doSomething();
+        Collaborator c = new Collaborator (){
+            public void doBusinessStuff(){
+            }
+        };
+        new ClassUnderTest(c).doSomething();
     }
 
 }
